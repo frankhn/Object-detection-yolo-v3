b3635cec3489ce2f511e451d65cb74dc30b374be
frankhn <harfrank2@gmail.com>
2021-06-24 19:27:21 +0200

    use coco  dataset

--- detection.py
+++ detection.py
@@ -3,9 +3,48 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
+whT = 320
+
+classesFile = 'coco.names'
+classNames = []
+
+with open(classesFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+# print(classNames)
+# print(len(classNames))
+
+modelConfiguration = 'yolov3-320.cfg'
+modelWeights = 'yolov3-320.weights'
+
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+def findObjects(outputs, img):
+    hT, wT, cT = img.shape
+    bbox = []
+    classIds = []
+    confs = []
 
 while True:
     success, img = cap.read()
+
+    blob = cv2.dnn.blobFromImage(img, 1/255, (whT, whT), [0, 0, 0], 1, crop=False)
+
+    net.setInput(blob)
+
+    layerNames = net.getLayerNames()
+    # print(layerNames)
+    outputNames = [layerNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    # print(outputNames)
+
+    # print(net.getUnconnectedOutLayers())
+
+    outputs = net.forward(outputNames)
+    print(len(outputs))
 
     cv2.imshow('Image', img)
     cv2.waitKey(1)
